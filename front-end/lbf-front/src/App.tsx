17c5e45c25745871b2209c2d97f709c043f952e0
Rhys Wong <RHYS0003@e.ntu.edu.sg>
2023-02-26 00:34:07 +0800

    Update web

--- front-end/lbf-front/src/App.tsx
+++ front-end/lbf-front/src/App.tsx
@@ -1,6 +1,7 @@
 import { testUpload } from './functions/test';
 import './App.css';
 import { Found } from './Components/Found';
+import { Whatever } from './Components/Whatever';
 
 const clickTest = () => {
   testUpload();
@@ -12,7 +13,9 @@ function App() {
     <div className="App">
       {/* <div onClick={clickTest}> Click me!! </div> */}
       <Found/>
+      <Whatever/>
     </div>
+    
   );
 }
 
